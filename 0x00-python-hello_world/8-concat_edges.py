cc8937f0ac20b009532d1d0f1a0525f2b739cf55
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-11-28 07:18:40 -0800

    python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
-print(str)
+str1 = "Python is an interpreted, interactive, object-oriented programming"
+str2 = " language that combines remarkable power with very clear syntax"
+print("object-oriented programming" + r" with Python")
