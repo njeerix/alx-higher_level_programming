b615d45683d6ffc0c193996cc3a65b0b65160c49
Michelle Njeeri <michellenjeeeri@gmail.com>
2023-12-21 00:19:58 -0800

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 
-
 class Square:
     """Class Square that defines a squre with a private size attribute."""
 
     def __init__(self, size):
         """Instantiation with a given size (no type/value verification)."""
         self.__size = size
+
+mysquare = Square(3)
+print(f"<class '1-square.Square'>")
+print(f"{{'_Square__size': {mysquare._Square__size}}}")
+
+mysquare = Square(89)
+print(f"<clas '1-square.Square'>")
+print(f"{{'_Square__size': {mysquare._Square__size}}}")
+
+try:
+    print(mysquare.size)
+except AttributeError as e:
+    print(e)
+
+try:
+    print(mysquare._Square__size)
+except AttributeError as e:
+    print(e)
