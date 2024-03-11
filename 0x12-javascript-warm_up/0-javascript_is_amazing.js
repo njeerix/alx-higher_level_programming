9e9e9c99cbbedab2811a3b8b82ab12b442b482ec
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-11 01:58:27 -0700

    JavaScript Warmup

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Create myVar constant variable with the value "JavaScript is amazing"
+const myVar = 'JavaScript is amazing';
+
+// Use console.log(...) to print the output
+console.log(myVar);
