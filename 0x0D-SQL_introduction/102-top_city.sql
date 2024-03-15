c439b639f3b8009f761a2b4452028bd71ad15592
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-15 04:13:29 -0700

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,10 +1,2 @@
--- Show tables in 'hbtn_0c_0' database
-SHOW TABLES FROM hbtn_0c_0;
-
 -- Selects the top 3 cities with the highest average temperature during July and August
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
