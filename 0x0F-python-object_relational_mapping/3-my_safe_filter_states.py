1ef22fa9c3b93e756a0a968880d16d951f78b4e2
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-15 06:54:52 -0700

    Python meets SQL

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+def filter_states(username, password, database, state_name):
+    """Lists all states with a name starting with 'N'"""
+    try:
+        # Connect to MySQL server running on localhost at port 3306
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        # Create a cursor object using cursor() method
+        cursor = db.cursor()
+        # Execute SQL queryy to select all states with a name starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (state_name,))
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+        # Display results
+        for row in rows:
+            print(row)
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys,argv[0]))
+        sys.exit(1)
+    username, password, database, state_name = sys.argv[1:]
+    filter_states(username, password, database, state_name)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a state from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+def filter_cities(username, password, database, state_name):
+    """Lists all cities of a state"""
+    try:
+        # Connect to MySQL server rnning on localhost at port 3306
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        # Create a cursor object using cursor() method
+        cursor = db.cursor()
+        # Execute SQL query to select all cities of the given state
+        cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id", (state_name,))
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+        # Display results
+        cities = ', '.join(row[0] for row in rows)
+        print(cities)
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MSQL Error:", e)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+    username, password, database, state_name = sys.argv[1:]
+    filter_cities(username, password, database, state_name)
