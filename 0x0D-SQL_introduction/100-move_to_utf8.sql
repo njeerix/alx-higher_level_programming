292b976f4e09a8a506b83483dff6a8fab28de6c1
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-15 03:36:33 -0700

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,5 +3,9 @@ USE hbtn_0c_0;
 
 -- Alter the database hbtn_0c_0 to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the first_table to UTF-8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Modify the name and score fields in first_table to specify only the collation
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
