f4a1efbb5246bf84161be9b135f65c3567967888
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-18 03:09:00 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -31,7 +31,7 @@ def filter_states(username, password, database):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys, argv[0]))
+        print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
     username, password, database = sys.argv[1:]
     filter_states(username, password, database)
