f088ba8e8059cf2f36b23c9479e07b56e1c4093f
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-18 04:00:58 -0700

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,6 +3,7 @@
 Lists all cities of a state from the database hbtn_0e_4_usa
 """
 
+
 import MySQLdb
 import sys
 
@@ -10,11 +11,14 @@ def filter_cities(username, password, database, state_name):
     """Lists all cities of a state"""
     try:
         # Connect to MySQL server rnning on localhost at port 3306
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database, port=3306)
         # Create a cursor object using cursor() method
         cursor = db.cursor()
         # Execute SQL query to select all cities of the given state
-        cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id", (state_name,))
+        cursor.execute("SELECT cities.name FROM cities JOIN states "
+                       "ON cities.state_id = states.id WHERE "
+                       "states.name = %s ORDER BY cities.id", (state_name,))
         # Fetch all rows from the result set
         rows = cursor.fetchall()
         # Display results
@@ -26,9 +30,11 @@ def filter_cities(username, password, database, state_name):
     except MySQLdb.Error as e:
         print("MSQL Error:", e)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        print("Usage: {} username password database state_name".format
+              (sys.argv[0]))
         sys.exit(1)
     username, password, database, state_name = sys.argv[1:]
     filter_cities(username, password, database, state_name)
