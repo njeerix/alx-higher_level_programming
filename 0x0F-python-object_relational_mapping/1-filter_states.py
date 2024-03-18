417f8b73944cf399b8c53cd9538dddf3213a57f3
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-18 03:05:29 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,11 +6,13 @@ Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
+
 def filter_states(username, password, database):
     """Lists all states with a name starting with 'N'"""
     try:
         # Connect to MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database, port=3306)
         # Create a cursor object using cursor() method
         cursor = db.cursor()
         # Execute SQL queryy to select all states with a name starting with 'N'
@@ -26,9 +28,10 @@ def filter_states(username, password, database):
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys,argv[0]))
+        print("Usage: {} username password database".format(sys, argv[0]))
         sys.exit(1)
     username, password, database = sys.argv[1:]
     filter_states(username, password, database)
