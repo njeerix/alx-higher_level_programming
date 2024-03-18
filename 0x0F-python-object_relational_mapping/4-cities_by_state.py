9d14d7c120b500c75f40a8801e3bb693882b6c9b
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-18 03:56:17 -0700

    update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,15 +6,19 @@ Lists all cities from the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
+
 def list_cities(username, password, database):
     """Lists all cities from the database"""
     try:
         # Conect to MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password,db=database, port=3306)
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database, port=3306)
         # Create a cursor object using cursor() method
         cursor = db.cursor()
         # Execute SQL query to select all cities sorted by cities.id
-        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER bY cities.id")
+        cursor.execute("SELECT cities.id, cities.name, "
+                       "states.name FROM cities JOIN states "
+                       "ON cities.state_id = states.id ORDER bY cities.id")
         # Fetch all ows from the result set
         rows = cursor.fetchall()
         # Display results
@@ -25,6 +29,7 @@ def list_cities(username, password, database):
         db.close()
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
