a21e819455ba74b9cfdf6787eebf6b1090c40256
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-03-18 04:56:06 -0700

    update

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa
+Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -8,21 +9,36 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-    # Create the connection engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    # Create a  configured "Session" class
-    Session = sessionmaker(bind=engine)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create a Session
-    session = Session()
+    try:
+        # Create the connection engine
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    # Query all state objects containing the letter 'a' and display them
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    if states:
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+        # Bind the engine to the Base class
+        Base.metadata.create_all(engine)
 
-    # Close the session
-    session.close()
+        # Create a  configured "Session" class
+        Session = sessionmaker(bind=engine)
+
+        # Create a Session
+        session = Session()
+
+        # Query all state objects containing the letter 'a' and display them
+        states = session.query(State)\
+            .filter(State.name.like('%a%'))\
+            .order_by(State.id)\
+            .all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+        # Close the session
+        session.close()
+
+    except Exception as e:
+        print("An error occurred:", e)
