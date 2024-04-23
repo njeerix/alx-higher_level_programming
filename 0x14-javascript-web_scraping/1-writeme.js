ec4c593ccb365e59a7657268e1ab570a04bbb5d3
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-04-23 08:30:43 -0700

    web-scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,22 +3,10 @@
 const fs = require('fs');
 
 // Retrieve the file path and string to write from command line arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Check if both file path and content arguments are provided
-if (!filePath || !content) {
-  console.error('Usage: ./1-writeme.js <file-path> "<string-to-write>"');
-  process.exit(1);
-}
+const file = process.argv[2];
+const string = process.argv[3];
 
 // Write the content to the specified file in utf-8 encoding
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    // Print the error object if an error occurred during writing
-    console.error(err);
-  } else {
-    // Output a success message if writing was successful
-    console.log(`Content was written to ${filePath}`);
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
