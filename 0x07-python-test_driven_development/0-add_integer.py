de028e825d14921c501a93d9b24d5466c99346b8
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-05-07 10:57:06 -0700

    python

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 def add_integer(a, b=None):
+    """
+    Adds two integers.
+
+    Prameters:
+       a (int or float): First integer or float.
+       b (int or float, optional): Second integer or float.
+
+    Returns:
+        int: The addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if b is not None and not isinstance(b, int) and not isinstance(b, float):
